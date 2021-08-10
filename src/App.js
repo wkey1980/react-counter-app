8bf20e1669f781e07d2c9563500ece5ccdda8f19
Will <28814921+wkey1980@users.noreply.github.com>
2021-08-10 17:10:26 +0100

    Built a working Count using React UseState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [count, setCount] = useState(0)
+
+	return (
+		<div className="app">
+			<div className="button-wrapper">
+				<h1>Count App</h1>
+				<h1>{count}</h1>
+				<button className="btn" onClick={()=>setCount(count-1)}>-</button>
+				<button className="btn" onClick={()=>setCount(count+1)}>+</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
